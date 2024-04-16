3a4e474f53af932bf7322881b6015da2c159a77e
surajpatwal22 <patwal0suraj@gmail.com>
2024-04-16 23:30:10 +0530

    admin

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('change_password', [UserController::class,'changePassword'])->name('change_password');
+    Route::get('get_profile', [UserController::class,'getProfile'])->name('get_profile');
+    Route::post('update_profile', [UserController::class,'updateProfile'])->name('update_profile');
+    Route::post('logout', [UserController::class,'logout'])->name('logout');
+
+    Route::middleware('admin')->group(function () {
+        
+       
+    });
 });
+
+Route::post('signup', [UserController::class,'signup'])->name('signup');
+Route::post('signin', [UserController::class,'signin'])->name('signin');
+Route::post('forgot_password', [UserController::class,'forgotPassword'])->name('forgot_password');
+Route::post('verify_otp', [UserController::class,'verifyOtp'])->name('verify_otp');
